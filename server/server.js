4c94a1b44977f2df325e33b167ddfe76d97240c3
Daniel Benson <dbenson24@gmail.com>
2020-04-04 13:19:45 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,6 +13,24 @@ let list = require('./controllers/list');
 let task = require('./controllers/task');
 let user = require('./controllers/user');
 
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://polished-breeze-0878.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'https://doitappuwf.herokuapp.com/auth',
+  issuer: 'https://polished-breeze-0878.auth0.com/',
+  algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
+app.get('/authorized', function (req, res) {
+res.send('Secured Resource');
+});
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
   response.sendFile(__dirname + '/index.html');
