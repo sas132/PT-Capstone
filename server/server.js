4ad1aaa7c935e8ee7c04ea7296373c218c0dba9a
Daniel Benson <dbenson24@gmail.com>
2020-04-04 14:02:22 -0500

    updated folder structure

--- server/server.js
+++ server/server.js
@@ -33,10 +33,12 @@ app.get('/authorized', function (req, res) {
   res.send('Secured Resource');
 });
 
+app.use('/', express.static('public'))
+
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + '/index.html');
-});
+// app.get("/", function(request, response) {
+//   response.sendFile(__dirname + '/index.html');
+// });
 
 app.get("/auth", (request, response) => {
   response.send("test");
