10b1a5a9cb9844d03647a4415d0d06aff6680a8d
Daniel Benson <dbenson24@gmail.com>
2020-04-04 16:07:11 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,41 +17,41 @@ let list = require('./controllers/list');
 let task = require('./controllers/task');
 let user = require('./controllers/user');
 
-const jwtCheck = jwt({
-  secret: jwks.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 500,
-    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
-  }),
-  audience: authConfig.audience,
-  issuer: `https://${authConfig.domain}/`,
-  algorithms: ['RS256']
-});
+// const jwtCheck = jwt({
+//   secret: jwks.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 500,
+//     jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+//   }),
+//   audience: authConfig.audience,
+//   issuer: `https://${authConfig.domain}/`,
+//   algorithms: ['RS256']
+// });
 
 app.use(express.static(join(__dirname, "public")));
 
-app.get("/auth_config.json", (req, res) => {
-  res.sendFile(join(__dirname, "auth_config.json"));
-});
+// app.get("/auth_config.json", (req, res) => {
+//   res.sendFile(join(__dirname, "auth_config.json"));
+// });
 
 app.get("/", (_, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
-app.get("/api/external", jwtCheck, (req, res) => {
-  res.send({
-    msg: "Your access token was successfully validated!"
-  });
-});
+// app.get("/api/external", jwtCheck, (req, res) => {
+//   res.send({
+//     msg: "Your access token was successfully validated!"
+//   });
+// });
 
-app.use(function(err, req, res, next) {
-  if (err.name === "UnauthorizedError") {
-    return res.status(401).send({ msg: "Invalid token" });
-  }
+// app.use(function(err, req, res, next) {
+//   if (err.name === "UnauthorizedError") {
+//     return res.status(401).send({ msg: "Invalid token" });
+//   }
 
-  next(err, req, res);
-});
+//   next(err, req, res);
+// });
 
 //add app user
 app.post('/user', user.add);
