c6fc80c856a9ae6d592f560ac09bf8fcb52f17b0
Daniel Benson <dbenson24@gmail.com>
2020-04-22 16:19:09 -0500

    Update user.js

--- services/user.js
+++ services/user.js
@@ -6,9 +6,11 @@ module.exports = {
 	//add new users to db
 	addUser: async function(userData) {
     try {
+      // check if user already exists
       const test = await User.find( { auth_id: { $eq: userData.auth_id } } ).exec()
       let result = false;
       if (test.length === 0) {
+        // add user if user doens't exist
         result = await User.create(userData)
       }
       return result;
